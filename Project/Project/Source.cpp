fe543b07a5325915568605bebf4d31641f339e39
ThanhLapTruong <thanhlap221202@gmail.com>
2023-11-13 16:28:30 +0700

    Nhap so

--- Project/Project/Source.cpp
+++ Project/Project/Source.cpp
@@ -1,2 +1,12 @@
-#include 
-using namespace
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cout << "Nhap so nguyen n: " << endl;
+	cin >> n;
+	system("pause");
+	return 0;
+}
